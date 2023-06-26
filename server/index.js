d6980982d8a4a0aee0a7e7f43e18a490af4a1261
S-P-Ks <kumbhareshubham9@gmail.com>
2023-06-26 12:05:15 +0530

    16 commit

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 // console.log(`${process.env.CLIENT_URL}`);
 app.use(
   cors({
-    origin: [`${process.env.CLIENT_URL}`, "http://localhost:3000/"],
+    origin: [`${process.env.CLIENT_URL}`],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
     allowedHeaders: ["Content-Type"],
